0bcae2c43ef6b274be65217f2fdfe21bb9ae33dd
cnugroho11 <cnugroho211@gmail.com>
2021-03-03 19:46:22 +0700

    new patch, new fonts, and more

--- config.def.h
+++ config.def.h
@@ -23,8 +23,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char font[]            = "Fira Code Retina 10";
+static const char dmenufont[]       = "Fira Code Retina:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -37,7 +37,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "WWW", "CODE", "TERM", "FILE", "ANY", "ANY", "DISC", "SPOT", "TELE" };
+static const char *tags[] = { "WWW", "CODE", "TERM", "FILE", "DOCS", "VIEW", "DISC", "SPOT", "TELE" };
 //static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 static const Rule rules[] = {
 	/* xprop(1):
@@ -48,6 +48,10 @@ static const Rule rules[] = {
 	{ "Gimp",               NULL,                   NULL,               0,            1,           -1 },
     { "TelegramDesktop",    NULL,                   NULL,               1 << 8,       1,           -1 },
     { "Spotify",            NULL,                   NULL,               1 << 7,       0,           -1 },
+    { "feh",                NULL,                   NULL,               1 << 5,       0,           -1 },
+    { "libreoffice",        NULL,                   NULL,               1 << 4,       0,           -1 },
+    { "discord",            NULL,                   NULL,               1 << 6,       0,           -1 },
+    { "Xreader",            NULL,                   NULL,               1 << 4,       0,           -1 },
 };
 
 /* layout(s) */
